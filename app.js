1458ad5843735d56d04ff033ca0e99cc8f31ca25
shivu <shivshankar.hc@gmail.com>
2018-09-05 19:46:16 +0000

    Modify app.js, used process.env

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var commentRoutes    = require("./routes/comments");
 var indexRoutes      = require("./routes/index");
 
 
-mongoose.connect("mongodb://shivu:shivu1998atmongolab@ds145752.mlab.com:45752/friendskart");
+mongoose.connect(process.env.database_url);
 
 //mongoose.connect("mongodb://localhost/friendskart_v15");//last element specifies to name of the database and this
 // line is used to create a database.
